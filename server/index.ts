f8f54faaf17edc9dd7eea622d199d6981e608ade
Doug Martin <dmartin@concord.org>
2025-07-02 06:13:16 -0400

    Added grading demo

--- server/index.ts
+++ server/index.ts
@@ -175,6 +175,31 @@ lti.onConnect(async (token, req, res) => {
 
       */
 
+    case "grading-demo":
+      const gradeLineItems  = await lti.Grade.getLineItems(token)
+      return res.send(`
+        <h2>Raw Grade Line Items</h2>
+        <pre>${JSON.stringify(gradeLineItems, null, 2)}</pre>
+
+        <form action="/grade" method="POST">
+          <input type="hidden" name="ltik" value="${req.token}" />
+          <label for="activityProgress">Activity Progress:</label>
+          <select name="activityProgress" id="activityProgress">
+            <option value="Initialized">Initialized</option>
+            <option value="Started">Started</option>
+            <option value="In Progress">In Progress</option>
+            <option value="Submitted">Submitted</option>
+            <option value="Completed">Completed</option>
+          </select>
+          <input type="submit" value="Update Activity Progress" />
+        </form>
+
+        <p>
+          NOTE: the activity progress change does get saved in Moodle, but we should test it in other LMSs.
+        </p>
+      `);
+      break
+
     default:
       return res.send("Unknown slug!")
   }
@@ -397,6 +422,46 @@ lti.app.get('/api/v1/jwt/firebase', async (req, res) => {
   })
 
   return res.status(200).send({token: firebaseToken});
+});
+
+lti.app.post('/grade', async (req, res) => {
+  const {activityProgress} = req.body;
+  if (!activityProgress) {
+    return res.status(400).send({ status: 400, error: 'Bad Request', details: { message: 'Missing required fields: activityProgress.' } });
+  }
+  const idToken = res.locals.token
+
+  // Creating Grade object
+  const gradeObj = {
+    userId: idToken.user,
+    scoreGiven: 99,
+    scoreMaximum: 100,
+    activityProgress,
+    gradingProgress: 'FullyGraded',
+  }
+
+  // Selecting linetItem ID
+  let lineItemId = idToken.platformContext.endpoint.lineitem // Attempting to retrieve it from idtoken
+  if (!lineItemId) {
+    const response = await lti.Grade.getLineItems(idToken, { resourceLinkId: true })
+    const lineItems = response.lineItems
+    if (lineItems.length === 0) {
+      // Creating line item if there is none
+      console.log('Creating new line item')
+      const newLineItem = {
+        scoreMaximum: 100,
+        label: 'Grade',
+        tag: 'grade',
+        resourceLinkId: idToken.platformContext.resource.id
+      }
+      const lineItem = await lti.Grade.createLineItem(idToken, newLineItem)
+      lineItemId = lineItem.id
+    } else lineItemId = lineItems[0].id
+  }
+
+  // Sending Grade
+  const responseGrade = await lti.Grade.submitScore(idToken, lineItemId, gradeObj)
+  return res.send(responseGrade)
 });
 
 const setup = async () => {
